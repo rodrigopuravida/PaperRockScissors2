8858ff846d9a738099615c1daa45f0c87a15aee4
Rodrigo Carballo <rodrigo.puravida@gmail.com>
2024-10-03 18:31:58 -0700

    Initial Commit

---
+++ PaperRockScissors2/PaperRockScissors2App.swift
@@ -0,0 +1,17 @@
+//
+//  PaperRockScissors2App.swift
+//  PaperRockScissors2
+//
+//  Created by Rodrigo Carballo on 10/3/24.
+//
+
+import SwiftUI
+
+@main
+struct PaperRockScissors2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
