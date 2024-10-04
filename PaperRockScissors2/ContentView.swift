88bf5f012777934d1beaf0a633d80fa8225a796e
Rodrigo Carballo <rodrigo.puravida@gmail.com>
2024-10-03 18:58:01 -0700

    Initial Commit

--- PaperRockScissors2/ContentView.swift
+++ PaperRockScissors2/ContentView.swift
@@ -8,16 +8,71 @@
 import SwiftUI
 
 struct ContentView: View {
+    let moves = ["✊", "✋", "✌️"]
+
+    @State private var computerChoice = Int.random(in: 0..<3)
+    @State private var shouldWin = Bool.random()
+    
+    @State private var score = 0
+    @State private var questionCount = 1
+    @State private var showingResults = false
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            VStack {
+                Spacer()
+
+                Text("Computer has played…")
+                    .font(.headline)
+                Text(moves[computerChoice])
+                    .font(.system(size: 200))
+
+                if shouldWin {
+                    Text("Which one wins?")
+                        .foregroundStyle(.green)
+                        .font(.title)
+                } else {
+                    Text("Which one loses?")
+                        .foregroundStyle(.red)
+                        .font(.title)
+                }
+
+                HStack {
+                    ForEach(0..<3) { number in
+                        Button(moves[number]) {
+                            
+                            
+                            
+                            
+                        }
+                        .font(.system(size: 80))
+                    }
+                }
+
+                Spacer()
+
+                Text("Score: ???")
+                    .font(.subheadline)
+
+                Spacer()
+            }
         }
-        .padding()
+    }
+    
+    func play(choice: Int) {
+        let winningMoves = [1, 2, 0]
+        let didWin: Bool
+
+        if shouldWin {
+            didWin = choice == winningMoves[computerChoice]
+        } else {
+            didWin = winningMoves[choice] == computerChoice
+        }
     }
 }
+
+
+
 
 #Preview {
     ContentView()
